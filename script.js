7220f34eae69c0bb5c76e9b3895840943348cc2f
Bhavyasree madineni <bhavyamadineni29@gmail.com>
2023-11-02 11:38:13 +0530

    image box

---
+++ script.js
@@ -0,0 +1,18 @@
+const boxesCont = document.getElementById('boxes');
+
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click',()=>boxesCont.classList.toggle('big'))
+
+function createBox(){
+    for(let i=0;i<4;i++){
+        for(let j=0;j<4;j++){
+            const box = document.createElement('div');
+            box.classList.add('box');
+            box.style.backgroundPosition = `${-j* 125}px ${-i*125}px`
+            boxesCont.appendChild(box);
+        }
+    }
+}
+
+createBox()
